3cbf63172afc29b43b62f26de503814150baa0c6
Márton Hartvig <34651552+hartvigmarton@users.noreply.github.com>
2020-09-24 12:52:46 +0200

    functionality extended,code rewritten

--- cafeteria.java
+++ cafeteria.java
@@ -46,31 +46,52 @@ What would you like today? (type 'how is business?' to attempt small talk, or 'n
 import java.util.Scanner;
 
 class Cafeteria {
-    String[] menu = new String[] { "Dagens", "Vegetar", "Halal" };
     double[] prices = new double[] {
-        53.90D, // for Dagens
-        42.50D, // for Vegetar
-        59.90D // for Halal
+            53.90D, // for Dagens
+            42.50D, // for Vegetar
+            59.90D // for Halal
     };
     double sum = 0D;
     double totalSum = 0D;
+    int nrDagens = 15;
+    int nrVegetar = 2;
+    int nrHalal = 5;
 
     void sell(String item) throws Exception {
         if (item.equals("Dagens")) {
-            sum = sum + prices[0];
-            totalSum = totalSum + prices[0];
-            System.out.println("Dagens, here you go.");
-        }
+            if (nrDagens > 0) {
+                sum = sum + prices[0];
+                totalSum = totalSum + prices[0];
+                nrDagens--;
+                System.out.println("Dagens, here you go.");
+            }
+            else {
+                System.out.println("Sorry, no more left of Dagens today.");
+            }
+            }
         else if (item.equals("Vegetar")) {
-            sum = sum + prices[1];
-            totalSum = totalSum + prices[1];
-            System.out.println("Vegetar, here you go.");
+            if (nrVegetar > 0) {
+                sum = sum + prices[1];
+                totalSum = totalSum + prices[1];
+                nrVegetar--;
+                System.out.println("Vegetar, here you go.");
+            }
+            else {
+                System.out.println("Sorry, no more left of Vegetar today.");
+            }
+
         }
         else if (item.equals("Halal")) {
-            sum = sum + prices[2];
-            totalSum = totalSum + prices[2];
-            System.out.println("Halal, here you go.");
+            if (nrHalal > 0) {
+                sum = sum + prices[2];
+                totalSum = totalSum + prices[2];
+                nrHalal--;
+                System.out.println("Halal, here you go.");
+            } else {
+                System.out.println("Sorry, no more left of Halal today.");
+            }
         }
+
         else {
             throw new Exception(item + " is not on the menu.");
         }
@@ -80,6 +101,17 @@ class Cafeteria {
         sum = 0D;
         System.out.println("Cash registry emptied. Thieves be warned!");
     }
+    void smalltalk(){
+        if (totalSum == 0) {
+            System.out.println("Not good so far. No one seems to be hungry today!");
+        }
+        else if ((totalSum > 0) && (totalSum <= 500)) {
+        System.out.println("Alright. Could have been better");
+        }
+        else if (totalSum > 500) {
+        System.out.println("Excellent! Lots of hungry students around today.");
+        }
+    }
 
     public static void main(String[] args) {
         Cafeteria fredrikke = new Cafeteria();
@@ -87,20 +119,12 @@ class Cafeteria {
         System.out.print("> ");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+
         while (!input.equals("not hungry")) {
-            boolean notAQuestion = false;
             if (input.equals("how is business?")) {
-                notAQuestion = true;
-                if (fredrikke.totalSum == 0) {
-                    System.out.println("Not good so far. No one seems to be hungry today!");
-                } else if (fredrikke.totalSum > 0) {
-                    System.out.println("Alright. Could have been better");
-                } else if (fredrikke.totalSum > 500) {
-                    // FIXME: this doesn't work properly
-                    System.out.println("Excellent! Lots of hungry students around today.");
-                }
+                fredrikke.smalltalk();
             }
-            if (!notAQuestion) {
+            else  {
                 try {
                     fredrikke.sell(input);
                 } catch (Exception e) {
